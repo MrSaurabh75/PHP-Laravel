c80a8b8074afae3cb9070980bb6da546e59bab31
unknown <saurabhdc7575@gmail.com>
2024-12-22 08:33:41 +0530

    Routing

--- myApp/routes/web.php
+++ myApp/routes/web.php
@@ -13,9 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/home', function () {
-    return view('home');
-});
-Route::get('/', function () {
+Route::get('/anything', function () {
     return view('welcome');
 });
+
+Route::get('/about/{name}',function($name){
+    // echo $name;
+    return view('about',['name'=>$name]);
+});
+
+// redirect
+// route::redirect('/home','/anything');
+
+// Route::get('/home', function () {
+//     return view('home');
+// });
+
+// OR
+
+Route::view('/home','home');
